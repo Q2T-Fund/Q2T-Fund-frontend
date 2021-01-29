fe9756c24b4be9e4656c219da2e21db0baabb011
carlosescom <carlos.escom.ipn@gmail.com>
2021-01-29 14:34:25 -0600

    components/BaseLayout: + Slot

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import { Slot } from 'react-page-layout';
 export default class BaseLayout extends Component {
   render() {
     return (
       <div>
-
+        <Slot name="page-content">
       </div>
     )
   }
