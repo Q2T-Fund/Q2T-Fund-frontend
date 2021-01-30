e9808de88058764c30a243fc8b7019a8b03185f4
carlosescom <carlos.escom.ipn@gmail.com>
2021-01-29 19:34:20 -0600

    components/BaseLayout: implement pushable sidebar

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -1,10 +1,63 @@
 import React, { Component } from 'react';
 import { Slot } from 'react-page-layout';
+import {
+  Button,
+  Icon,
+  Menu,
+  Segment,
+  Sidebar,
+} from 'semantic-ui-react'
+
+const VerticalSidebar = ({ animation, direction, visible }) => (
+  <Sidebar
+    as={Menu}
+    animation={animation}
+    direction={direction}
+    icon='labeled'
+    inverted
+    vertical
+    visible={visible}
+    width='thin'
+  >
+    <Menu.Item as='a'>
+      <Icon name='home' />
+      Home
+    </Menu.Item>
+    <Menu.Item as='a'>
+      <Icon name='gamepad' />
+      Games
+    </Menu.Item>
+    <Menu.Item as='a'>
+      <Icon name='camera' />
+      Channels
+    </Menu.Item>
+  </Sidebar>
+)
+
 export default class BaseLayout extends Component {
+
+  componentDidMount() {
+    this.setState({
+      animation: 'push',
+      direction: 'left',
+      dimmed: false,
+      visible: false,
+    })
+  }
+
   render() {
     return (
       <div>
-        <Slot name="page-content">
+        <Sidebar.Pushable as={Segment} style={{ overflow: 'hidden' }}>
+          <VerticalSidebar
+            animation={this.state.animation}
+            direction={this.state.direction}
+            visible={this.state.visible}
+          />
+          <Sidebar.Pusher dimmed={this.state.dimmed && this.state.visible}>
+            <Slot name="page-content" />
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
       </div>
     )
   }
