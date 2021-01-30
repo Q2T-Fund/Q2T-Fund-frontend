302cd98453ab6ee7754b52ed42019c9cf66a4c33
carlosescom <carlos.escom.ipn@gmail.com>
2021-01-30 01:44:21 -0600

    components/BaseLayout: setup layout according to wireframes

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -4,8 +4,9 @@ import {
   Button,
   Icon,
   Menu,
-  Segment,
+  Container,
   Sidebar,
+  Image
 } from 'semantic-ui-react'
 
 const VerticalSidebar = ({ animation, direction, visible }) => (
@@ -19,17 +20,24 @@ const VerticalSidebar = ({ animation, direction, visible }) => (
     visible={visible}
     width='thin'
   >
+    <Container>
+      <Image src={"https://cdn.animaapp.com/projects/60126ea786f83e0fcc799456/releases/60126ec431580128926bc3d9/img/photo-1@1x.png"}/>
+    </Container>
     <Menu.Item as='a'>
       <Icon name='home' />
-      Home
+      Dashboard
     </Menu.Item>
     <Menu.Item as='a'>
-      <Icon name='gamepad' />
-      Games
+      <Icon name='line graph' />
+      Your Stats
     </Menu.Item>
     <Menu.Item as='a'>
-      <Icon name='camera' />
-      Channels
+      <Icon name='bell' />
+      Notifications
+    </Menu.Item>
+    <Menu.Item as='a'>
+      <Icon name='setting' />
+      Settings
     </Menu.Item>
   </Sidebar>
 )
@@ -48,14 +56,22 @@ export default class BaseLayout extends Component {
 
   render() {
     return (
-      <Sidebar.Pushable style={{ overflow: 'hidden'}} style={{ height: '100vh' }}>
+      <Sidebar.Pushable style={{ overflow: 'hidden', height: '100vh', width: '100vw' }}>
         <VerticalSidebar
           animation={this.state.animation}
           direction={this.state.direction}
           visible={this.state.visible}
         />
-        <Sidebar.Pusher
-          dimmed={this.state.dimmed && this.state.visible}>
+        <Sidebar.Pusher dimmed={this.state.dimmed && this.state.visible}>
+          <div style={{
+            height: '100vh', backgroundColor: '#1b1c1d', padding: '1em', width: '5em', display: 'inline-block'
+          }}>
+            <Button onClick={() => this.setState({ visible: !this.state.visible })}>
+              <Button.Content>
+                <Icon name='content' />
+              </Button.Content>
+            </Button>
+          </div>
           <Slot name="page-content" />
         </Sidebar.Pusher>
       </Sidebar.Pushable>
