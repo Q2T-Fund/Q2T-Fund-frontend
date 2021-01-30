b1e1f1fffd79d63964edd10481ce55a51054999b
carlosescom <carlos.escom.ipn@gmail.com>
2021-01-30 01:44:41 -0600

    components/DummyPage: try to implement grid, no success yet

--- src/components/DummyPage.js
+++ src/components/DummyPage.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import { Page, Section } from 'react-page-layout';
+import { Grid, Image } from 'semantic-ui-react';
 
+const GridExampleRows = () => (
+  <Grid columns={3}>
+    <Grid.Row>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+      </Grid.Column>
+    </Grid.Row>
+
+    <Grid.Row>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+)
 export default class DummyPage extends Component {
   render() {
     return (
       <Page layout="base">
         <Section slot="page-content">
-          <h1> THIS IS THE PAGE CONTENT </h1>
+          <GridExampleRows/>
         </Section>
       </Page>
     )
