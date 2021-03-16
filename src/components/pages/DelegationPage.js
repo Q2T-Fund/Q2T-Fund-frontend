1f5909dec399e1a0cc6ec0ce72edb093c348d46b
mchrupcala <michael.chrupcala@gmail.com>
2021-03-16 12:26:25 -0400

    Split Delegate into differrent components and renamed

--- src/components/pages/DelegationPage.js
+++ src/components/pages/DelegationPage.js
@@ -1,188 +1,12 @@
 import React, { useState } from "react";
 import Card from '../common/Card';
 import { Page, Section } from "react-page-layout";
-import { Form, Input, Slider, Radio } from "formik-antd";
-import { Formik } from "formik";
-import { approveDai } from "../../api/contracts";
-import { openNotification } from "../utils/common-functions";
-import { validateKovanNet, depositTx, getTreasuryDAOAddress } from '../../api/contracts';
-
+import DelegateForm from "./DelegateForm";
 import "antd/dist/antd.css";
 import "../css/DelegationPage.css";
 import "../css/BaseLayout.css";
 
 require("dotenv").config();
-
-
-const ContractInteraction = (props) => {
-  const address = getTreasuryDAOAddress(props.template);
-
-  return (
-    <>
-      <Formik
-        initialValues={{
-          tokenAmount: 0,
-          repaymentPercent: 0,
-          totalReturn: 0,
-          currency: "DAI",
-        }}
-
-        validate={(values) => {
-          const errors = {};
-          if (!values.tokenAmount) {
-            errors.tokenAmount = "Required";
-          } else if (values.tokenAmount <= 0) {
-            errors.tokenAmount = "Provide a valid amount.";
-          }
-          return errors;
-        }}
-
-        onSubmit={async (values, { setSubmitting }) => {
-          const isKovan = await validateKovanNet();
-          if (!isKovan) {
-            openNotification(
-              "Transaction Failed!",
-              `Please switch to Kovan network before proceeding.`,
-              false
-            );
-            return;
-          }
-          await approveDai(address, values.tokenAmount);
-          await depositTx(
-            address,
-            values.currency,
-            values.tokenAmount,
-            values.repaymentPercent
-          );
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-          /* and other goodies */
-        }) => (
-          <Form onSubmit={handleSubmit}>
-            <>
-              <div className="x01b-delegation-agreement dove-gray-border-1px">
-                <div className="title raleway-bold-black-22px">
-                  Your Delegation Agreement
-                </div>
-                <div className="overlap-group">
-                  <img
-                    className="path-1491"
-                    src="https://anima-uploads.s3.amazonaws.com/projects/60126ea786f83e0fcc799456/releases/60126ec431580128926bc3d9/img/path-1491@1x.png"
-                  />
-                  <p className="by-delegating-the-tr raleway-normal-black-14px-2">
-                    <span className="span">
-                      By delegating the Treasury, you can fund and support
-                      projects for the Public Goods. The Quadratic Treasury will
-                      be “locking” these funds to provide “
-                    </span>
-                    <span className="span1-DQxoii">non-repayable loans</span>
-                    <span className="span">
-                      ” to these projects using Quadratic Funding and a
-                      milestone-based approach to prevent any form of fraud and
-                      collusion.
-                    </span>
-                  </p>
-                </div>
-                <div className="auto-flex6">
-                  <div className="auto-flex">
-                    <div className="amount raleway-semi-bold-black-18px">
-                      Amount
-                    </div>
-
-                    <Input
-                      type="number"
-                      name="tokenAmount"
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.tokenAmount}
-                      placeholder="5000"
-                      className="overlap-group1"
-                    />
-                    {<div>{errors.tokenAmount}</div>}
-
-                    <div className="group-1330">
-                      <Radio.Group
-                        name="currency"
-                        onBlur={handleBlur}
-                        value={values.currency}
-                      >
-                        <Radio value={"DAI"}>DAI</Radio>
-                        <Radio value={"USDC"}>USDC</Radio>
-                      </Radio.Group>
-                    </div>
-                  </div>
-                  <div className="auto-flex">
-                    <div className="repayment-structure raleway-semi-bold-black-18px">
-                      Repayment Structure
-                    </div>
-                    <p className="text-1 raleway-normal-black-14px">
-                      Decide whether you want it to be a full donation, or
-                      receive back part of your funds.
-                    </p>
-
-                    <div className="auto-flex1">
-                      <div className="number-1 raleway-bold-black-14px">0</div>
-
-                      <Slider
-                        name="repaymentPercent"
-                        min={0}
-                        max={50}
-                        color="black"
-                        onBlur={handleBlur}
-                        value={values.repaymentPercent}
-                        className="repayment-percent-bar"
-                      />
-
-                      <div className="number-2 raleway-bold-black-14px">50</div>
-                    </div>
-
-                    <div className="repayment-percent-number raleway-normal-black-14px">
-                      {`${values.repaymentPercent} % Repayment Percent`}
-                    </div>
-                  </div>
-                  <div className="auto-flex">
-                    <div className="your-return raleway-semi-bold-black-18px">
-                      Your Return
-                    </div>
-                    <p className="text-2 raleway-normal-black-14px">
-                      This is how much you will receive back from your funds.
-                      Plus interest!
-                    </p>
-                    <div className="auto-flex4">
-                      <div className="rectangle-621-1"></div>
-                      <div className="price raleway-bold-black-14px">{`${(
-                        (values.repaymentPercent / 100) *
-                        values.tokenAmount
-                      ).toFixed(0)} USD`}</div>
-                    </div>
-                  </div>
-                </div>
-                <div className="submit-button-container">
-                  <button type="submit" className="submit-button">
-                    Delegate & Support!
-                  </button>
-                </div>
-              </div>
-
-              <div className="message-container">
-                <div className="success-font">{null}</div>
-              </div>
-            </>
-          </Form>
-        )}
-      </Formik>
-    </>
-  );
-};
-
 
 const DelegationPage = () => {
   const [template, setTemplate] = useState('open-source');
@@ -241,7 +65,7 @@ const DelegationPage = () => {
       </Section>
 
       <Section slot="row2-col1">
-        <ContractInteraction template={template} />
+        <DelegateForm template={template} />
       </Section>
     </Page>
   );
